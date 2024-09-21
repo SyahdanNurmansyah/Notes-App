8bfe5c6e2dc8c9fb819d1aa9c82ac5a1394dd144
Syahdan Nurmansyah <syahdan.nurmansyah@gmail.com>
2024-09-21 21:56:24 +0700

    fix bugs

--- src/api/exports/index.js
+++ src/api/exports/index.js
@@ -1,11 +1,11 @@
-const ProducerHandler = require("./handler");
+const ExportsHandler = require("./handler");
 const routes = require("./routes");
 
 module.exports = {
     name: 'exports',
     version: '1.0.0',
     register: async (server, { service, validator }) => {
-        const exportsHandler = new ProducerHandler(service, validator);
+        const exportsHandler = new ExportsHandler (service, validator);
 
         server.route(routes(exportsHandler));
     },
